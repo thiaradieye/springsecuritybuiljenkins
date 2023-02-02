72d53da338502085dbbb02cf95d8caa77d755972
thiaradieye <thiaradieye@gmail.com>
2023-02-02 19:42:04 +0000

    first commit

---
+++ src/main/java/com/groupisi/springsecuritybuiljenkins/SpringsecuritybuiljenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.groupisi.springsecuritybuiljenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybuiljenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybuiljenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupisi/springsecuritybuiljenkins/SpringsecuritybuiljenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupisi.springsecuritybuiljenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybuiljenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
